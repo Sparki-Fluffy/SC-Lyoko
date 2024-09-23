3d50e51a0165a380a620d4e64e363d03517557c7
Sparki-Fluffy <andrewdev276@gmail.com>
2024-09-23 11:57:39 +0700

    Добавлен подмодуль библиотеки SFML.

---
+++ bin/CMakeFiles/SC-Lyoko.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sparky/Documents/Проекты/SC-Lyoko/src/App.cpp" "CMakeFiles/SC-Lyoko.dir/src/App.cpp.o" "gcc" "CMakeFiles/SC-Lyoko.dir/src/App.cpp.o.d"
+  "/home/sparky/Documents/Проекты/SC-Lyoko/src/Base.cpp" "CMakeFiles/SC-Lyoko.dir/src/Base.cpp.o" "gcc" "CMakeFiles/SC-Lyoko.dir/src/Base.cpp.o.d"
+  "/home/sparky/Documents/Проекты/SC-Lyoko/src/Button.cpp" "CMakeFiles/SC-Lyoko.dir/src/Button.cpp.o" "gcc" "CMakeFiles/SC-Lyoko.dir/src/Button.cpp.o.d"
+  "/home/sparky/Documents/Проекты/SC-Lyoko/src/Layout.cpp" "CMakeFiles/SC-Lyoko.dir/src/Layout.cpp.o" "gcc" "CMakeFiles/SC-Lyoko.dir/src/Layout.cpp.o.d"
+  "/home/sparky/Documents/Проекты/SC-Lyoko/src/Menu.cpp" "CMakeFiles/SC-Lyoko.dir/src/Menu.cpp.o" "gcc" "CMakeFiles/SC-Lyoko.dir/src/Menu.cpp.o.d"
+  "/home/sparky/Documents/Проекты/SC-Lyoko/src/Scene.cpp" "CMakeFiles/SC-Lyoko.dir/src/Scene.cpp.o" "gcc" "CMakeFiles/SC-Lyoko.dir/src/Scene.cpp.o.d"
+  "/home/sparky/Documents/Проекты/SC-Lyoko/src/main.cpp" "CMakeFiles/SC-Lyoko.dir/src/main.cpp.o" "gcc" "CMakeFiles/SC-Lyoko.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
