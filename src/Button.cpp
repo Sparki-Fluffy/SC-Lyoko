105a016836148c80c8916d5aa2cc7ed3f6a40f45
Sparki-Fluffy <153190463+Sparki-Fluffy@users.noreply.github.com>
2024-09-23 12:07:33 +0700

    Revert "Add interface"

--- src/Button.cpp
+++ src/Button.cpp
@@ -1,78 +1,66 @@
 #include "../include/Button.hpp"
 
 Button::Button(std::string name, float x, float y, float width, float height,
-               sf::Color color, sf::Color selectedColor, Asset<sf::Texture>* texture,
-               Asset<sf::Texture>* selectedTexture, float rotation, float originX,
-               float originY, sf::String text, Asset<sf::Font>* font, 
+               sf::Color color, sf::Color selectedColor, sf::Texture* texture,
+               sf::Texture* selectedTexture, float rotation, float originX,
+               float originY, sf::String text, sf::Font* font, 
                unsigned int fontSize, sf::Color textColor, relativePosition textPosition) :
         Base(name, x, y, width, height, color, selectedColor,
              texture, selectedTexture, rotation, originX, originY)
 {
-    if (font != nullptr) setText(text, *font, fontSize);
-    setTextColor(textColor);
-    setTextPosition(textPosition);
+        setText(text, *font, fontSize);
+        setTextColor(textColor);
+        setTextPosition(textPosition);
 }
 
-std::string Button::getType() const
-{
+std::string Button::getType() const {
     return "Button";
 }
 
-sf::Vector2f Button::getTextPosition()
-{
+sf::Vector2f Button::getTextPosition() {
     return textPosition;
 }
 
-void Button::setTextColor(sf::Color color)
-{
+void Button::setTextColor(sf::Color color) {
     textColor = color;
 }
 
-void Button::setText(sf::String text, Asset<sf::Font>& font, unsigned int size)
-{
+void Button::setText(sf::String text, sf::Font& font, unsigned int size) {
     this->text.setString(text);
-    this->font = font;
-    this->text.setFont(this->font);
+    this->text.setFont(font);
     this->text.setCharacterSize(size);
     this->textSize.x = this->text.getLocalBounds().width;
     this->textSize.y = this->text.getLocalBounds().height;
 }
 
-void Button::setText(sf::Text text)
-{
+void Button::setText(sf::Text text) {
     this->text = text;
     this->textSize.x = this->text.getLocalBounds().width;
     this->textSize.y = this->text.getLocalBounds().height;
 }
 
-void Button::setTextPosition(relativePosition position)
-{
+void Button::setTextPosition(relativePosition position) {
     textRelativePosition = position;
 }
 
-void Button::setTextPosition(sf::Vector2f position)
-{
+void Button::setTextPosition(sf::Vector2f position) {
     this->textPosition = position;
 }
 
-void Button::setFunction(std::function<void()> onClick)
-{
+void Button::setFunction(std::function<void()> onClick) {
     this->onClick = onClick;
 }
 
-sf::Text Button::getText()
-{
+sf::Text Button::getText() {
     return text;
 }
 
-void Button::draw(sf::RenderWindow& window)
-{
+void Button::draw(sf::RenderWindow& window) {
     Base::draw(window);
 
     sf::Vector2f pos;
 
-    switch (textRelativePosition)
-    {
+    switch (textRelativePosition) {
         case None:
              pos = textPosition;
              break;
@@ -120,10 +108,10 @@ void Button::draw(sf::RenderWindow& window)
     text.setPosition(sf::Vector2f(position.x + textPosition.x, position.y + textPosition.y));
     text.setFillColor(textColor);
     window.draw(text);
+
 }
 
-void Button::onEvent(sf::Event& event)
-{
+void Button::onEvent(sf::Event& event) {
     switch (event.type)
     {
         case sf::Event::KeyPressed:   
@@ -137,15 +125,12 @@ void Button::onEvent(sf::Event& event)
     }
 }
 
-void Button::onKeyPressed(sf::Event::KeyEvent& key)
-{
-    if (isSelected && key.code == Controls["Use"].first || key.code == Controls["Use"].second)
-    {
+void Button::onKeyPressed(sf::Event::KeyEvent& key) {
+    if (isSelected && key.code == Controls["Use"].first || key.code == Controls["Use"].second) {
         onClick();
     }
 }
 
-void Button::onKeyReleased(sf::Event::KeyEvent& key)
-{
+void Button::onKeyReleased(sf::Event::KeyEvent& key) {
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,53 @@
 // Main file
 #include "../include/LyokoInterface.hpp"
-
-Scene& makeScene(App& app)
-{
-    
-    Asset<sf::Font> font = Fonts["gun4f"];
-    Asset<sf::Texture> texture = Textures[1];
-
-    Menu* cont = new Menu
-    (
-        "Menu", VMenu, 100, 0, 0,
-        texture.getSize().x * 5 + 400,
-        texture.getSize().y * 5 + 400, sf::Color::Green
-    );
-    for (int i = 0; i < 5; i++)
-    {
-        Menu* m = new Menu
-        (
-            "Submenu " + std::to_string(i), HMenu, 100, 0, 0,
-            texture.getSize().x * 5 + 400,
-            texture.getSize().y, sf::Color::Red
-        );
-        cont->add(m);
-        for (int j = 0; j < 5; j++)
-        {
-            m->add(new Button
-                (
-                    "Button cont " + std::to_string(j), 0, 100 * i,
-                    texture.getSize().x, texture.getSize().y,
-                    sf::Color::White, sf::Color::Red,
-                    &texture, &texture,
-                    0, 0, 0,
-                    "hah", &font, 20, sf::Color::Red,
-                    Center
-                )
-            );
-            m->get<Button>(j)->setFunction(std::bind(&App::close, &app));
-        }
-    }
-    cont->select();
-    Scene* scene = new Scene("Out");
-    scene->add(cont);
-
-    return *scene;
-}
+/*void close(sf::RenderWindow* window) {
+    window->close();
+}*/
 
 int main()
 {
-    App mainApp;
-    Scene scene("Out");
+    nlohmann::json j;
+    sf::Font font;
+    if (!font.loadFromFile("../resource/fonts/gunshipv4/gun4f.ttf")) {
+
+    }
+
+    sf::Text text, text2;
+        
+    text.setFont(font);    
+    text.setString("Hello");
+    text.setCharacterSize(20);
     
-    //scene.loadFromFile("../scenes/out.json");
-    scene = makeScene(mainApp);
-    scene.saveToFile("../scenes/out.json");
-    mainApp.setScene(scene);
+    text2.setFont(font);
+    text2.setString("hah");
+    text2.setCharacterSize(20);
+
+    sf::String path = "../resource/img/Bottom-panel-2-vers.png";
+    sf::Texture texture;
+    texture.loadFromFile(path);
+	const uint8_t FPS = 60;
+    App mainApp;
+    Menu cont("Menu", VMenu, 100, 0, 0, texture.getSize().x * 5 + 400, texture.getSize().y * 5 + 400, sf::Color::Green);
+    for (int i = 0; i < 5; i++) {
+        Menu* m = new Menu("Submenu " + std::to_string(i), HMenu, 100, 0, 0, texture.getSize().x * 5 + 400, texture.getSize().y, sf::Color::Red);
+        cont.addChild(m);
+        for (int j = 0; j < 5; j++) {
+            m->addChild(new Button("Button cont " + std::to_string(j), 0, 100 * i, texture.getSize().x, 
+                          texture.getSize().y, sf::Color::White, sf::Color::Red, &texture,
+                          &texture, 0, 0, 0, "hah", &font, 20,
+                          sf::Color::Red, Center));
+            m->getChild<Button>("Button cont " + std::to_string(j))->setFunction(std::bind(&App::close, &mainApp));
+        }
+    }
+    cont.select();
+    cont.getChild<Menu>("Submenu 1")->select();
+    cont.getChild<Menu>("Submenu 1")->getChild<Button>("Button cont 0")->select();
+    std::cout << (long long)cont.getChild<Menu>("Submenu 1")->getChild<Button>("Button cont 0") << "\n";
+    Scene scene("Out");
+    scene.loadFromFile("../scenes/out.json");
+   // scene.addChild(&cont);
+   // scene.saveToFile("../scenes/out.json");
+    mainApp.setScene(scene); 
     mainApp.run();
 	return 0;
 }
